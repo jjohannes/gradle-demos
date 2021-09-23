f1a9d85c52d138628bb27819a52d3c102243f12b
Jendrik Johannes <jendrik.johannes@gmail.com>
2021-09-23 16:56:38 +0200

    Update Java17 sample to use GA release

--- java-17/build-logic/java-plugins/src/main/kotlin/java17-library.gradle.kts
+++ java-17/build-logic/java-plugins/src/main/kotlin/java17-library.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     id("java-library")
 }
 
-// Make sure Java 17 JDK is discovered by Gradle
-// E.g. by setting 'org.gradle.java.installations.paths' in gradle.properties
-// See also: https://docs.gradle.org/current/userguide/toolchains.html#sec:custom_loc
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 dependencies {
--- java-17/lib/build.gradle.kts
+++ java-17/lib/build.gradle.kts
@@ -1,5 +1,3 @@
 plugins {
-    // Put java toolchain config in a local convention plugin to use it in all (sub)projects
-    // See: build-logic/java-plugins/src/main/kotlin/java17-library.gradle.kts
     id("java17-library")
 }
